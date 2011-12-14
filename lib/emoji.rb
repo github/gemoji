629c149e5b03424af81ef6b98c72a0219718aa15
Trevor Turk <trevorturk@gmail.com>
2011-12-14 17:18:55 -0600

    array not hash

--- lib/emoji.rb
+++ lib/emoji.rb
@@ -9,7 +9,7 @@ module Emoji
   def self.replace(string)
     string.gsub(/:([a-z0-9\+\-_]+):/) do |message|
       name = $1.to_s.downcase
-      if names[name]
+      if names.include?(name)
         %(<span class="emoji emoji-#{name}" title=":#{name}:"></span>)
       else
         message
