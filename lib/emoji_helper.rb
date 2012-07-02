f7841e2b6e982b85713fb65f17cb0f367a88d955
Trevor Turk <trevorturk@gmail.com>
2012-07-02 17:09:17 -0500

    ruby 1.8 compat hash syntax

--- lib/emoji_helper.rb
+++ lib/emoji_helper.rb
@@ -2,7 +2,7 @@ module EmojiHelper
   def emojify(content)
     h(content).to_str.gsub(/:([a-z0-9\+\-_]+):/) do |match|
       if Emoji.names.include?($1)
-        image_tag("emoji/#{$1}.png", size: "20x20", style: "vertical-align:middle")
+        image_tag("emoji/#{$1}.png", :size => "20x20", :style => "vertical-align:middle")
       else
         match
       end
