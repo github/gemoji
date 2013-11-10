13bcea9b4a798ee9d5cefd70cc4064e387300b68
Javan Makhmali <javan@javan.us>
2013-11-10 11:09:48 -0500

    Make mappings private pending better names

--- lib/emoji.rb
+++ lib/emoji.rb
@@ -25,42 +25,43 @@ module Emoji
     inverted_mapping[unicode]
   end
 
-  def mapping
-    @mapping ||= {}.tap do |mapping|
-      emoji_path = "#{images_path}/emoji"
+  private
+    def mapping
+      @mapping ||= {}.tap do |mapping|
+        emoji_path = "#{images_path}/emoji"
 
-      Dir["#{emoji_path}/*.png"].each do |filename|
-        name = File.basename(filename, ".png")
+        Dir["#{emoji_path}/*.png"].each do |filename|
+          name = File.basename(filename, ".png")
 
-        if File.symlink?(filename)
-          unicode_filename = "#{emoji_path}/#{File.readlink(filename)}"
-          mapping[name] = []
+          if File.symlink?(filename)
+            unicode_filename = "#{emoji_path}/#{File.readlink(filename)}"
+            mapping[name] = []
 
-          loop do
-            codepoints = unicode_filename.match(/unicode\/([\da-f\-]+)\.png/)[1]
-            mapping[name] << codepoints.split("-").map(&:hex).pack("U*")
+            loop do
+              codepoints = unicode_filename.match(/unicode\/([\da-f\-]+)\.png/)[1]
+              mapping[name] << codepoints.split("-").map(&:hex).pack("U*")
 
-            if File.symlink?(unicode_filename)
-              unicode_filename = "#{emoji_path}/unicode/#{File.readlink(unicode_filename)}"
-            else
-              break
+              if File.symlink?(unicode_filename)
+                unicode_filename = "#{emoji_path}/unicode/#{File.readlink(unicode_filename)}"
+              else
+                break
+              end
             end
+          else
+            mapping[name] = nil
           end
-        else
-          mapping[name] = nil
         end
       end
     end
-  end
 
-  def inverted_mapping
-    @inverted_mapping ||= {}.tap do |inverted_mapping|
-      mapping.each do |name, unicodes|
-        next if unicodes.nil?
-        unicodes.each do |unicode|
-          inverted_mapping[unicode] = name
+    def inverted_mapping
+      @inverted_mapping ||= {}.tap do |inverted_mapping|
+        mapping.each do |name, unicodes|
+          next if unicodes.nil?
+          unicodes.each do |unicode|
+            inverted_mapping[unicode] = name
+          end
         end
       end
     end
-  end
 end
